d3421085142416eb4689fece47b9cf2e1b0a7037
Yarbmw234 <yaroslavvnukov66@gmail.com>
2023-09-21 17:47:03 -0600

    updates

--- main.py
+++ main.py
@@ -1,71 +1,113 @@
 import random
 from random import choice
-things = [
-    "rock",
-    "paper",
-    "scissors",
-    "spock",
-    "lizard"
-]
 
-for i in range(5):
-    print(things[i])
+playing = True
 
-userChoose = input("Select your thing: ")
+games = int(0)
+win_text = "You won"
+wins_count = int(0)
+lose_text = "You lose"
+loses_count = int(0)
 
-botChoose = random.choice(things)
+while playing:
 
-print("Your choose:", userChoose)
-print("Bot choose:", botChoose)
+    things = [
+        "rock",
+        "paper",
+        "scissors",
+        "spock",
+        "lizard"
+    ]
 
-def rock():
-    if botChoose == "scissors" or botChoose == "lizard":
-        print("You won!")
-    elif botChoose == "rock":
-        print("Dead heat")
+    for i in range(5):
+        print(things[i])
+
+    userChoose = input("Select your thing: ").lower()
+
+    if userChoose == "":
+        playing = False
+        quit()
+
+    botChoose = random.choice(things)
+
+    print("Your choose:", userChoose)
+    print("Bot choose:", botChoose)
+
+    def rock():
+        global wins_count
+        global loses_count
+        if botChoose == "scissors" or botChoose == "lizard":
+            print(win_text)
+            wins_count += 1
+        elif botChoose == "rock":
+            print("Dead heat")
+        else:
+            print(lose_text)
+            loses_count += 1
+
+    def paper():
+        global wins_count
+        global loses_count
+        if botChoose == "rock" or botChoose == "spock":
+            print(win_text)
+            wins_count += 1
+        elif botChoose == "paper":
+            print("Dead heat")
+        else:
+            print(lose_text)
+            loses_count += 1
+
+    def scissors():
+        global wins_count
+        global loses_count
+        if botChoose == "paper" or botChoose == "lizard":
+            print(win_text)
+            wins_count += 1
+        elif botChoose == "scissors":
+            print("Dead heat")
+        else:
+            print(lose_text)
+            loses_count += 1
+
+    def spock():
+        global wins_count
+        global loses_count
+        if botChoose == "scissors" or botChoose == "rock":
+            print(win_text)
+            wins_count += 1
+        elif botChoose == "spock":
+            print("Dead heat")
+        else:
+            print(lose_text)
+            loses_count += 1
+
+    def lizard():
+        global wins_count
+        global loses_count
+        if botChoose == "spock" or botChoose == "paper":
+            print(win_text)
+            wins_count += 1
+        elif botChoose == "lizard":
+            print("Dead heat")
+        else:
+            print(lose_text)
+            loses_count += 1
+
+
+    if userChoose == "rock":
+        rock()
+    elif userChoose == "paper":
+        paper()
+    elif userChoose == "scissors":
+        scissors()
+    elif userChoose == "spock":
+        spock()
+    elif userChoose == "lizard":
+        lizard()
     else:
-        print("You lose")
+        print("Don't enter something else!")
 
-def paper():
-    if botChoose == "rock" or botChoose == "spock":
-        print("You won!")
-    elif botChoose == "paper":
-        print("Dead heat")
-    else:
-        print("You lose")
+    games += 1
+    print("GAMES:", games, "WINS:", wins_count, "LOSES:", loses_count)
+    print()
 
-def scissors():
-    if botChoose == "paper" or botChoose == "lizard":
-        print("You won!")
-    elif botChoose == "scissors":
-        print("Dead heat")
-    else:
-        print("You lose")
-
-def spock():
-    if botChoose == "scissors" or botChoose == "rock":
-        print("You won!")
-    elif botChoose == "spock":
-        print("Dead heat")
-    else:
-        print("You lose")
-
-def lizard():
-    if botChoose == "spock" or botChoose == "paper":
-        print("You won!")
-    elif botChoose == "lizard":
-        print("Dead heat")
-    else:
-        print("You lose")
-
-
-if userChoose == "rock":
-    rock()
-elif userChoose == "paper":
-    paper()
-elif userChoose == "scissors":
-    scissors()
-elif userChoose == "spock":
-    spock()
-elif userChoose == "lizard":
-    lizard()
